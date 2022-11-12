73ad4d96bb99be12dd36772d2f05babe2d8d0bb6
M-A-R-V-E-L-A <87486824+M-A-R-V-E-L-A@users.noreply.github.com>
2022-11-12 20:53:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup() {
+    canvas = createCanvas(550, 500);
+    canvas.center();
+    canvas.position(560, 150);
+  
+    video = createCapture(VIDEO);
+    video.position(180, 100);
+    video.size(550, 500);
+  
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+  }
+  
+  function draw() {
+    background("#30D5C8");
+  }
+  
+  function modelLoaded() {
+    console.log("PoseNet Is Initialized!");
+  }
+  
+  function gotPoses(results) {
+    if (results.length > 0) {
+      console.log(results);
+    }
+  }
+  
